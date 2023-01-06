0bde095abc09688d8b5eba00d83fbe864052340a
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-06 17:08:48 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -8,12 +8,27 @@ namespace TestMAxNumOf3Var
         {
             Console.WriteLine("****** Welcome to Find the Max Num of 3 Variables Program ******");
 
-            string X = "Apple";
-            string Y = "Peach";
-            string Z = "Banana";
+            int X = 9;
+            int Y = 22;
+            int Z = 35;
 
-            string val = FindMax<string>.MaxOfString(X, Y, Z);
-            Console.WriteLine("The greater String is " + val);
+            int obj = FindMax<int>.MaxOfString(X, Y, Z);
+            Console.WriteLine("The greater Int Num is " + obj);
+
+            float A = 8.3F;
+            float B = 3.4F;
+            float C = 1.2F;
+
+            float val = FindMax<float>.MaxOfString(A, B, C);
+            Console.WriteLine("The greater Float Num  is " + val);
+
+
+            string L = "Apple";
+            string M = "Peach";
+            string N = "Banana";
+
+            string val7 = FindMax<string>.MaxOfString(L, M, N);
+            Console.WriteLine("The greater String is " + val7);
         }
     }
 }
