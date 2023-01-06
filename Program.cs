abfbd55ef3a8000ab182c45b4e084c90a63ab961
dhanashripatil205 <dhanashripatil205@gmail.com>
2023-01-06 16:21:17 +0530

    Method calling

--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
-﻿namespace TestMAxNumOf3Var
+﻿using TestMaxNumOf3Var;
+
+namespace TestMAxNumOf3Var
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("****** Welcome to Find the Max Num of 3 Variables Program ******");
+
+            int X = 20;
+            int Y = 40;
+            int Z = 60;
+
+            int val = FindMax<int>.MaxIntNumber(X, Y, Z);
+            Console.WriteLine("The greater num is " + val);
         }
     }
 }
